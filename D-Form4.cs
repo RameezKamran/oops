e4948feb10e01c28c3953c3540028d685f624cab
RameezKamran <rameezkamran01@gmail.com>
2025-05-20 17:01:36 +0500

    Create D-Form4.cs
    Form4.designer.cs file hai ye

---
+++ D-Form4.cs
@@ -0,0 +1,186 @@
+namespace EduQuestFinal
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.codingchallenge = new System.Windows.Forms.Button();
+            this.typemonk = new System.Windows.Forms.Button();
+            this.brainteaser = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.Grip;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::EduQuestFinal.Properties.Resources.gamers_using_different_devices_playing_mobile_phone_tablet_laptop_console_cartoon_illustration;
+            this.pictureBox1.Image = global::EduQuestFinal.Properties.Resources.gamers_using_different_devices_playing_mobile_phone_tablet_laptop_console_cartoon_illustration;
+            this.pictureBox1.Location = new System.Drawing.Point(125, -4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(554, 265);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Old Antic Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(249, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 47);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Welcome";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(457, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "abc";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCoral;
+            this.button1.BackgroundImage = global::EduQuestFinal.Properties.Resources._360_F_301975843_AQKFRF1qr7U2x3gYK4WtgqAcT6aNaDaa;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(430, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Codeaway";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // codingchallenge
+            // 
+            this.codingchallenge.BackColor = System.Drawing.Color.SpringGreen;
+            this.codingchallenge.BackgroundImage = global::EduQuestFinal.Properties.Resources._new;
+            this.codingchallenge.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.codingchallenge.FlatAppearance.BorderSize = 2;
+            this.codingchallenge.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.codingchallenge.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Bold);
+            this.codingchallenge.ForeColor = System.Drawing.Color.Black;
+            this.codingchallenge.Location = new System.Drawing.Point(215, 327);
+            this.codingchallenge.Margin = new System.Windows.Forms.Padding(0);
+            this.codingchallenge.Name = "codingchallenge";
+            this.codingchallenge.Size = new System.Drawing.Size(194, 44);
+            this.codingchallenge.TabIndex = 4;
+            this.codingchallenge.Text = "Coding Challenge";
+            this.codingchallenge.UseVisualStyleBackColor = false;
+            this.codingchallenge.Click += new System.EventHandler(this.codingchallenge_Click);
+            // 
+            // typemonk
+            // 
+            this.typemonk.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.typemonk.BackgroundImage = global::EduQuestFinal.Properties.Resources.Woodstock_background_III_min;
+            this.typemonk.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.typemonk.FlatAppearance.BorderSize = 2;
+            this.typemonk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.typemonk.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Bold);
+            this.typemonk.Location = new System.Drawing.Point(430, 327);
+            this.typemonk.Name = "typemonk";
+            this.typemonk.Size = new System.Drawing.Size(178, 41);
+            this.typemonk.TabIndex = 5;
+            this.typemonk.Text = "TypeMonkey";
+            this.typemonk.UseVisualStyleBackColor = false;
+            this.typemonk.Click += new System.EventHandler(this.typemonk_Click);
+            // 
+            // brainteaser
+            // 
+            this.brainteaser.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.brainteaser.FlatAppearance.BorderSize = 2;
+            this.brainteaser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.brainteaser.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.brainteaser.Image = global::EduQuestFinal.Properties.Resources.wer;
+            this.brainteaser.Location = new System.Drawing.Point(224, 400);
+            this.brainteaser.Margin = new System.Windows.Forms.Padding(0);
+            this.brainteaser.Name = "brainteaser";
+            this.brainteaser.Size = new System.Drawing.Size(185, 41);
+            this.brainteaser.TabIndex = 6;
+            this.brainteaser.Text = "Brain Teaser";
+            this.brainteaser.UseVisualStyleBackColor = false;
+            this.brainteaser.Click += new System.EventHandler(this.brainteaser_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.BackgroundImage = global::EduQuestFinal.Properties.Resources.istockphoto_1371310881_612x612;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.brainteaser);
+            this.Controls.Add(this.typemonk);
+            this.Controls.Add(this.codingchallenge);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form4";
+            this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button codingchallenge;
+        private System.Windows.Forms.Button typemonk;
+        private System.Windows.Forms.Button brainteaser;
+
+
+    }
+   
+
+
+}
